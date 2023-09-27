e5a86c555c0a66562fa4fcde3aefcba62e906a25
Justin Huynh <justinhuynh639@gmail.com>
2023-09-27 12:06:10 -0700

    started qr code generator

---
+++ app.py
@@ -0,0 +1,20 @@
+#install all the libraries needed
+import qrcode
+
+#create a function that collects a text and converts it to a qr code
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    #save the qr code as an image
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+    
+#run the function
